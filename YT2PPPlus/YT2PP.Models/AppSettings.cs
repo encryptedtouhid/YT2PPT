d8288875fb48a3132bfe4d16e6e279395a78bb97
Khaled <me@tuhidulhossain.com>
2024-06-19 22:54:12 +0800

    NuGet Added

---
+++ YT2PPPlus/YT2PP.Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YT2PP.Models
+{
+    public class AppSettings
+    {
+
+        public string Version { get; set; }
+
+        public string YouTubeApiKey { get; set; }
+
+        public TimeSpan FreeLimit { get; set; }
+    }
+}
